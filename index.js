c12863103b9ee1809a3c77ddf2cfb4990fdfd8e3
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-31 11:04:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ var url = process.env.MONGODB_URI;
 
 const app = express();
 
+config = {
+  chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca", // 32 byte (64 char) hex string
+  keyProvider: process.env.key, // WIF string or array of keys..
+  httpEndpoint: 'http://193.93.219.219:8888',
+  expireInSeconds: 60,
+  broadcast: true,
+  verbose: false, // API activity
+  sign: true
+}
+
+eos = Eos(config);
+
+async function transfer2(from, to, amount, memo){
+	const myaccount = await eos.contract(from);
+	await myaccount.transfer(from, to, amount + " " + "DAB",memo);
+}
 
 function initAirDrop(){
 	MongoClient.connect(url, (err, db) => {
@@ -20,10 +36,36 @@ function initAirDrop(){
 
 const airdrop = async() => {
 	console.log("do airdrop");
+	const msg = "Dabble(https://dabble.cafe) is a social Dapp which rewards users for writings. You can trade DAB token at ********** ********** *****. This airdrop is supported by ********** ********** *******."
+	MongoClient.connect(url, (err, db) => {
+		const dbo = db.db("heroku_23gbks9t");
+		const findQuery = { drop : "false" };
+		dbo.collection('snapshot0824').findOne(findquery, function(err, res){
+			if(res.length != 0){
+				transfer2("eoscafekorea", res.account, "1000.0000", msg).then((output)=>{
+					//update db to true
+					const findQuery = {_id : ObjectId(res._id)};
+					const myObj = {$set : {drop : true}};
+					dbo.collection('snapshot0824').updateOne(findQuery, myObj, function(err, resUpdate){
+						console.log("airdrop completed for", res.account);
+						db.close();
+					});
+					
+			}
+		});
+	});
 }
-
-airdrop();
-initAirDrop();
+eos.transaction(tr => {
+	  tr.buyrambytes({
+    payer: 'eoscafekorea',
+    receiver: 'eoscafekorea',
+    bytes: 20000*1024
+  })
+});
+	
+//airdrop();
+setInterval(airdrop, 30);						       
+//initAirDrop();
 	
 
 
