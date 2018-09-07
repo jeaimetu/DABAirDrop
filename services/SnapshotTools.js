3c10fd04d6302b08f129f6922482727a1daff834
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-08 02:06:10 +0900

    Update SnapshotTools.js

--- services/SnapshotTools.js
+++ services/SnapshotTools.js
@@ -25,8 +25,8 @@ exports.getCSV = (pathToCSV) => {
 
 const checkAccountVote = async(account) => {
     const accountInfo = await EOSTools.getAccount(account); // replace this to eostool?
-    console.log("checkAccountVote", accountInfo, accountInfo.voter_info.producers.length, 
-               accountInfo.voter_info.producers.length.proxy);
+    //console.log("checkAccountVote", accountInfo, accountInfo.voter_info.producers.length, 
+    //           accountInfo.voter_info.producers.length.proxy);
     if ( accountInfo.voter_info.proxy == "" || !accountInfo.voter_info.producers)
         return false;
     else
