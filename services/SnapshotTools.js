62205da15943f3480eb12407382adc427bd6dc07
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-08 01:12:41 +0900

    Update SnapshotTools.js

--- services/SnapshotTools.js
+++ services/SnapshotTools.js
@@ -45,7 +45,7 @@ const checkAccountVote2 = async(account) => {
 
 
 exports.test = async(tupled) => {
-    MongoClient.connect(url, (err, db) => {
+    async r = MongoClient.connect(url, (err, db) => {
         let finalResult = [];
     for(let i = 0; i < tupled.length; i++){
         const isVote = await checkAccountVote(tupled[i].account);
