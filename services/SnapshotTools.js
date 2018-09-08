c599036629ff113bcfeea2034c0c25d7199f09e2
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-08 10:39:50 +0900

    Update SnapshotTools.js

--- services/SnapshotTools.js
+++ services/SnapshotTools.js
@@ -69,7 +69,7 @@ const checkAccountVote2 = async(account) => {
              
             const myObj = {account : tupled[i].account, amount :  amount, idx : i};
             const res = await db.collection('snapshot0907').insertOne(myObj);
-            //console.log(`res => ${JSON.stringify(res)}`);
+            console.log("capturing", tupled[i].account, amount, i);
  
         }
         client.close();  
