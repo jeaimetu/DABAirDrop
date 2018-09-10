31c23e4027b2b71b1eec040233e2cbe8fad822a3
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-10 10:03:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Eos = require('eosjs');
 const app = express();
 
 //airdrop suspend
-//return;
+return;
 
 config = {
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // 32 byte (64 char) hex string
