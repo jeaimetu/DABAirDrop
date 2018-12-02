1637f64abf68007a50e3ae0f071d4904b2faa27f
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-12-02 13:54:25 +0900

    Update airdrop.js

--- airdrop.js
+++ airdrop.js
@@ -146,7 +146,7 @@ const run = async () => {
     //chintai testing(S)
     console.log("calling chintai list");
     chintai = require('./chintai/s1');
-    console.log("chintai length", chinta.length);
+    console.log("chintai length", chintai.length);
     for(i = 0;i<chintai.length;i++){
         console.log("chintai", chintai[i].user, chintai[i].quantity);
         //removing EOS and change it with ParseFloat
